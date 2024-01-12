8772f345d95e39b3ee4a65dc988d5a34625cdf7b
Веселин Инзов <veselin.inzov@gmail.com>
2024-01-12 12:41:53 +0200

    fixes

--- src/main/java/com/book/library/BookLibrary/services/UserServiceImpl.java
+++ src/main/java/com/book/library/BookLibrary/services/UserServiceImpl.java
@@ -23,7 +23,7 @@ public class UserServiceImpl implements  UserService{
     @Autowired
     private Mapper mapper;
 
-    public void UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, Mapper mapper) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, Mapper mapper) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.mapper = mapper;
